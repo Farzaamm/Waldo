1c731dd66b9a93c83c0ee68d2c398fc4858e5ed7
Farzaamm <farzamkhonamri@gmail.com>
2026-05-06 06:22:43 +0000

    add waldoFinder to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,120 +1,33 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+// import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // coordinates logger to determin the coordinates of Waldo in the image, this will be used to set the coordinates of Waldo in the database
+  const waldoFinder = (event) => { 
+    const rect = event.currentTarget.getBoundingClientRect() // getBoundingClientRect() returns the size of an element and its position relative to the viewport.
+    const xOnScreen  = event.clientX - rect.left // clientX and clientY are the coordinates of the mouse pointer relative to the top-left corner of the element
+    const yOnScreen = event.clientY - rect.top // this ensures that the coordinates are relative to the image, not the entire page
+    
+    const x = Math.round((xOnScreen / rect.width) * 100) // Convert to percentage
+    const y = Math.round((yOnScreen / rect.height) * 100) // Convert to percentage
+    // console.log(`Coordinates: (${x}, ${y})`)
+
+    const topLeftX = 61 
+    const topLeftY = 35 
+    const bottomRightX = 62 
+    const bottomRightY = 36
+
+    if (x >= topLeftX && x <= bottomRightX && y >= topLeftY && y <= bottomRightY) {
+      alert('Congratulations! You found Waldo!')
+    } else {
+      alert('Try again! Waldo is not at these coordinates.')
+    }
+  }
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+      <h1>Welcome to Waldo App</h1>
+      <img src="Beach scene.jpg" onClick={waldoFinder} alt="" />
     </>
   )
 }
