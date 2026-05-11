d211c1d31d858843a55dc1b8cb78d0d5cfea0813
Farzaamm <farzamkhonamri@gmail.com>
2026-05-11 06:38:32 +0000

    add img of waldo to Landing.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='bg-gray-800 text-white text-center p-4'>
+    <div className='bg-gray-800 text-white text-center p-4 z-50 relative'>
         <h1>Created by <a href="https://github.com/Farzaamm" target="_blank" rel="noopener noreferrer">Farzaamm</a></h1>
     </div>
   )
